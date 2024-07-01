a8ffa86c6067f60b5082e8a21ac58b652d04d214
Mahaveer A <135404390+Mahaveer1013@users.noreply.github.com>
2024-07-01 21:02:02 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://pr-management-system.vercel.app',
     withCredentials: true, // Send cookies with requests
 });
 
