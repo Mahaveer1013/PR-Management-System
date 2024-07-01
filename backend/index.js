99e9cb128b74202c4dd773d5e0499ad01c4b987d
Mahaveer A <135404390+Mahaveer1013@users.noreply.github.com>
2024-07-01 20:58:34 +0530

    cors modify

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'http://mahaveer-pr-management-system',
   credentials:true
 }));
 app.use(cookieParser())
